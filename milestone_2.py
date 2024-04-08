1bf861f7b65f884bcffb3619122a34fb0424079b
shay-94 <akshayptl26@gmail.com>
2024-04-08 11:40:38 +0100

    Added word list and conditional to check guess

--- milestone_2.py
+++ milestone_2.py
@@ -1,1 +1,14 @@
+import random
+import string
 
+word_list = ['apple', 'banana', 'orange', 'grape', 'strawberry']
+
+word = random.choice(word_list)
+
+guess = input("Enter guess: ").lower()
+
+if len(guess) == 1 and guess in string.ascii_lowercase:
+    print('Good guess')
+    print(word, f'Your guess was: {guess}')
+else:
+    print("Oops that's not a valid input")
