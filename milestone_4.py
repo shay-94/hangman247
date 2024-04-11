44d9d5aba77cd40073e37ed08cf9e8cca6df92a8
shay-94 <akshayptl26@gmail.com>
2024-04-11 17:47:02 +0100

    Refactoring and optimising

--- milestone_4.py
+++ milestone_4.py
@@ -4,11 +4,11 @@ class Hangman:
     def __init__(self, word_list) -> None:
         self.num_lives = 5
         self.word = random.choice(word_list)
-        self.word_guessed = ['_' for x in self.word]
+        self.word_guessed = ['_' for letter in self.word]
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
         
-    def check_guess(self, guess):
+    def _check_guess(self, guess):
         #Checks if the guess this round is in the answer word
         if guess in self.word:
             print(f"Good guess! {guess} is in the word")
@@ -17,7 +17,7 @@ class Hangman:
             print(f"Sorry, {guess} is not in the word. Try again. You have {self.num_lives} lives left")
             self.num_lives -=1
             
-    def ask_for_input(self):
+    def _ask_for_input(self):
     #Gets players guess and checks its a letter and then whether it's correct
         while True:
             #Asks user for their guess this round
@@ -27,9 +27,9 @@ class Hangman:
                 if guess not in self.list_of_guesses:
                     self.check_guess(guess)
                     self.list_of_guesses.append(guess)
-                    for x,v  in enumerate(self.word):
-                        if guess == v:
-                            self.word_guessed[x] = v
+                    for count,letter  in enumerate(self.word):
+                        if guess == letter:
+                            self.word_guessed[count] = letter
                     print(self.num_letters, self.word_guessed)
                 else:
                     print("You already tried that letter!")
